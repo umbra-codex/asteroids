7963d9ca20cdbe74581e095132f6b5a7aabf969b
UmbraCodex <the.shadow.skript@gmail.com>
2026-01-21 14:41:06 -0500

    D: added & completed the movement of the player

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
 import pygame
 from logger import log_state
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
 
     while True:
         log_state()
-
-        clock = pygame.time.Clock()
-        dt = 0
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
-        pygame.display.flip()
 
-        clock.tick(60)
         dt = clock.tick(60) / 1000
 
+        updatable.update(dt)
 
+        for sprite in drawable:
+            sprite.draw(screen)
 
-    print(f"Starting Asteroids with pygame version: ", pygame.version.ver)
-    print(f"Screen width:", SCREEN_WIDTH)
-    print(f"Screen height:", SCREEN_HEIGHT)
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
