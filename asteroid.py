1e8f8d0b8a52aeee77e4948900fdbacd56e39f2e
UmbraCodex <the.shadow.skript@gmail.com>
2026-01-24 06:08:58 -0500

    F: added the ability for the asteroids to split

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 from circle_shape import CircleShape
-from constants import LINE_WIDTH
+from logger import log_event
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
 
 class Asteroid(CircleShape):
   def __init__(self, x, y, radius):
@@ -11,3 +13,22 @@ class Asteroid(CircleShape):
 
   def update(self, dt):
     self.position += self.velocity * dt
+
+  def split(self):
+    self.kill()
+
+    if self.radius <= ASTEROID_MIN_RADIUS:
+      return
+    
+    log_event("asteroid_split")
+
+    angle = random.uniform(20, 50)
+    left_velocity = self.velocity.rotate(angle)
+    right_velocity = self.velocity.rotate(-angle)
+    new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+    first_asteroid_split = Asteroid(self.position.x, self.position.y, new_radius)
+    first_asteroid_split.velocity = left_velocity * 1.2
+    second_asteroid_split = Asteroid(self.position.x, self.position.y, new_radius)
+    second_asteroid_split.velocity = right_velocity * 1.2
+
--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def main():
             for shot in shots:
                 if shot.collides_with(asteroid):
                     log_event("asteroid_shot")
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         for sprite in drawable:
